69ff8bae1cfe104983d4cc805887a89e44039087
Kevin Baichoo <kbaichoo4078@bths.edu>
2014-03-14 00:20:21 -0400

    Added navbar, set up basic master layout,included bootstrap and jQuery

--- physics-db/app/views/master.blade.php
+++ physics-db/app/views/master.blade.php
@@ -1,13 +1,23 @@
 <!doctype html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>{{ "Pass title here!" }}</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-		<!-- Yield nav here! -->
+	@include('nav')
+
+
 	<div class="container">
 		@yield('content')
 	</div>
+
 
 </body>
 </html>
--- physics-db/app/views/nav.blade.php
+++ physics-db/app/views/nav.blade.php
@@ -1,27 +1,29 @@
- <h3 class="navbar navbar-default">
- 	<ul class="navbar-nav navbar-static-top">
- 		<li class="grid_2">
- 			<a href="index1.php">Index</a>
- 		</li>
- 		<li class="grid_2">
- 			<a href="equipment.php">Equipment</a>
- 		</li>
- 		<li class="grid_2">
- 			<a href='admin.php'>My Account!</a>
- 		</li>
-		<?php if(isset($_SESSION['user']) && ($_SESSION['level'] == "admin") || ($_SESSION['level'] == "superadmin")){ ?>
-		
-			<li class="grid_2">
-				<a href='admin.php?manage=accounts'>Manage Accounts</a>
-			</li class="grid_2">
-			<li class="grid_2">
-				<a href='admin.php?manage=items'>Manage Items</a>
-			</li>
-		
-		<?php } ?>
- 		
- 		<li class="grid_1">
- 			<a href="logout.php">Logout</a>
- 		</li>
- 	</ul>
- </h3>
+ <nav class="navbar navbar-default" role="navigation">
+  <div class="container">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+ 				<a href="index1.php">Index</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="equipment.php">Equipment</a></li>
+          <li><a href='admin.php'>My Account!</a></li>
+          <!-- if logged in and admin -->
+          <li><a href='admin.php?manage=accounts'>Manage Accounts</a></li>
+					<li><a href='admin.php?manage=items'>Manage Items</a></li>
+					<li><a href="logout.php">Logout</a></li>
+          
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </div>
+</nav>
