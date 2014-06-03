6bdaba50f8bafe20588474091e2df01b7788b35e
Jake Christensen <jakec43@gmail.com>
2014-06-03 11:19:45 -0400

    implementing changes to equipment

--- equipment.php
+++ equipment.php
@@ -1,125 +1,108 @@
 <?php
-
+	//fuck optimization, I do what I want
 	$needDB = true;
 	include('inc/header.php');
 
 
-	$queryStatement = "select name,id from items";
+	$queryStatement = "select * from items where id = " . $_GET["id"] . ";";
 	$query = $conn->prepare($queryStatement);
 	$query->execute();
 
+	$item = $query->fetch(PDO::FETCH_ASSOC);
+
+	$queryStatement = "select * from containers where id in (select container_id from container_item_assignments where item_id = " . $_GET["id"] . ");";
+	$query = $conn->prepare($queryStatement);
+	$query->execute();
+
+	$containers = $query->fetchAll(PDO::FETCH_ASSOC);
+
+	function get_section($a)
+	{
+		return $a["section_id"];
+	}
+	$queryStatement = "select * from sections where id in (" . join(",", array_map("get_section", $containers)) . ");";
+	$query = $conn->prepare($queryStatement);
+	$query->execute();
+
+	$sections = $query->fetchAll(PDO::FETCH_ASSOC);
+
+	function get_room($a)
+	{
+		return $a["room_id"];
+	}
+	$queryStatement = "select * from rooms where id in (" . join(",", array_map("get_room", $sections)) . ");";
+	$query = $conn->prepare($queryStatement);
+	$query->execute();
+
+	$rooms = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
 		<style type="text/css">
-@import url(http://fonts.googleapis.com/css?family=Lora);
-#container{
-    width:960px;
-    border:1px solid black;
-    font:28px Lora;
-    margin:auto;
-    text-align:center;
-}
-
-.equipment{
-    border:1px solid red;
-    width:350px;
-}
-.item_info{
-		    border:1px solid black;
-		    display:inline-block;
-		    margin-left:2px;
-		    width:500px;
-		    left:375px;
-		    position:relative;
-}
-
-		.closeBtn{
-		    position:absolute;
-		    top:0px;
-		    right:0px;
-		    color:lightgrey;
-		    font-size:18px;
-		    z-index: 2;
-		}
-		.closeBtn:hover{
-		    color:grey;
-		}
-
-		.description{
-		    display:block;
-		}
-
-		.locationsTop{
-		    display:block;
-		    text-align:center;
-		    width:350px;
-		    margin:auto;
-		}
-
-		.locationInfo{
-		    display:block;
-		}
+			.indent {
+				margin-left:50px;
+			}
+			.description {
+				margin-left:20px;
+			}
 		</style>
-		<script type="text/javascript">
-			function makeElement(element,innerHTML,className,id){
-				theElement = document.createElement(element);
-				theElement.innerHTML = innerHTML;
-				if(className){
-					theElement.setAttribute("class",className);
-				}
-				if(id){
-					theElement.setAttribute("id",id);
-				}
-				return theElement;
-			}
-
-			function toggleInfo(itemId,element){
-				if($(element).find(".item_info").length === 0 ){
-					var url = "ajax.php?function=itemDetails&itemId=" + itemId;
-					$.get(url).done(function(data){
-
-						itemData = JSON.parse(data);
-						//Makes container for information
-						var container = makeElement('div',"",'item_info');
-						
-						//makes button to toggle close
-						var closeBtn = makeElement('span','X','closeBtn');
-						closeBtn.setAttribute('onclick','close(' + itemId + ');');
-						container.appendChild(closeBtn);
-
-						//Description of the item
-						var description = makeElement("span","Description:",'description');
-						description.innerHTML += itemData[0];
-						if(itemData[0] == ""){
-							description.innerHTML += "No Description Avaliable";
-						}
-						container.appendChild(description);
-						
-
-						var locations = makeElement('span','Quantity x Location','locationsTop');
-						for(var i = 0; i < itemData[1].length; i++){
-							var currData = itemData[1][i];
-							var str = currData['quantity'] + ' X ' + currData['room']['room_number']  + ' in section ' + currData['room']['section_name'] + ' in ' + currData['section']['container_type']  + ' with label ' + currData['section']['container_label'];
-							locations.appendChild(makeElement('span',str,'locationInfo'));
-						}
-						container.appendChild(locations);
-						element.appendChild(container);
-					});
-				}
-			}
-			
-			/*function close(parentNumber){
-				$($(".equipment")[parentNumber - 1]).hide();
-			}*/
-		</script>
 	</head>
 	<body>
-		<div id="container">
-		<?php 
-			while($item = $query->fetch(PDO::FETCH_ASSOC)) {
-				echo "<div class='equipment' onclick='toggleInfo(".$item['id'] .",this)'>" . $item['name'] . "</div>";
-			}
+		<?php
+			include('inc/nav_bar.php');
 		?>
+		<div class="indent">
+			<div class="item">
+				<h3>
+					<?php echo($item['name']); ?>
+				</h3>
+				<div class='description'>
+					<?php echo($item['description']); ?>
+				</div>
+				<h4>
+					In room(s): 
+				</h4>
+				<div class='rooms'>
+					<?php 
+						foreach($rooms as $room)
+						{
+							echo("<div class='room'>");
+							echo($room["name"] . ", ");
+							$room_sections = [];
+							for($i = 0; $i<count($sections); $i++)
+							{
+								$section = $sections[$i];
+								$section_containers = [];
+								if($section["room_id"] == $room["id"])
+								{
+									foreach($containers as $container)
+									{
+										if($container["section_id"] == $section["id"])
+										{
+											array_push($section_containers, $container["type"] . " " . $container["label"]);
+										}
+									}
+									if(count($section_containers) > 1)
+									{
+										array_push($room_sections, $section["name"] . ", containers " . join(", ", $section_containers));
+									}
+									else
+									{
+										array_push($room_sections, $section["name"] . ", container " . $section_containers[0]);
+									}
+								}
+							}
+							if(count($room_sections) > 1)
+							{
+								echo("sections " . join(", ", $room_sections));
+							}
+							else
+							{
+								echo("section " . $room_sections[0]);
+							}
+							echo("</div>");
+						}
+					?>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
